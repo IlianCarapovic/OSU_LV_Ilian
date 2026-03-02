80667aa24e8630c1ee5ff2139bc56952a31e64e8
IlianCarapovic <iliancarapovic@Ilians-MacBook-Air.local>
2026-03-02 12:33:08 +0100

    fix zad 1.4.2

--- LV1/Zad_1.4.2.py
+++ LV1/Zad_1.4.2.py
@@ -1,15 +1,20 @@
+try:
+    number = float(input())
 
-grade = float(input())
-while grade < 0.0 or grade > 1.0:
-    grade = float(input())
+    if number < 0.0 or number > 1:
+        print("Invalid number")
+        exit()
 
-if grade >= 0.9:
-    print("A")
-elif grade >= 0.8:
-    print("B")
-elif grade >= 0.7:
-    print("C")
-elif grade >= 0.6:
-    print("D")
-else:
-    print("F")
+    if number >= 0.9:
+        print("A")
+    elif number >= 0.8:
+        print("B")
+    elif number >= 0.7:
+        print("C")
+    elif number >= 0.6:
+        print("D")
+    else:
+        print("F")
+
+except ValueError:
+    print("Enter a valid number between 0.0 and 1.0")
